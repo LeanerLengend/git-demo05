1d739f7ef2abbb66e16ef1a4794bb933a910043b
ChenYingxiang <1919577366@qq.com>
2023-03-10 14:28:07 +0800

    master

--- src/com/lucky/git/SoutMany.java
+++ src/com/lucky/git/SoutMany.java
@@ -9,7 +9,7 @@ public class SoutMany {
 
     public static void main(String[] args) {
         System.out.println("Hello World -- change");
-        System.out.println("my name is combat!!");
+        System.out.println("this is master!");
     }
 
 
